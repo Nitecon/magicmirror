c6fda83b6dcaaaa699e3b7e3734cdf1e326a4bbe
Will Hattingh <nitecon@gmail.com>
2022-10-16 14:10:50 -0400

    omti dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ do
         ln -s /data/mm/modules/$i /data/MagicMirror/modules/$i
         echo "installing node modules for $i"
         cd /data/mm/modules/$i
-        npm install && npm update
+        npm install --omit-dev && npm update
 done
 cp -f /data/mm/config/* /data/MagicMirror/config/
 
